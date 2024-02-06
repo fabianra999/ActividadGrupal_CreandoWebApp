029e161faad8922c6bfbf7a6e6407d939d2f0ded
Mauricio Herrera <johnmaohd@gmail.com>
2024-02-06 16:14:48 -0500

    ajustes

--- js/loginform.js
+++ js/loginform.js
@@ -1,17 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Verifica si estamos en la página de inicio y si hay una sesión activa
     if (window.location.pathname.includes('home.html')) {
         const userEmail = sessionStorage.getItem('userEmail');
         if (userEmail) {
             document.getElementById('userEmail').textContent = userEmail;
         } else {
-            // Redirige al login si no hay sesión activa
             window.location.href = 'home.html';
         }
 
         document.getElementById('logoutBtn').addEventListener('click', function() {
-            sessionStorage.removeItem('userEmail'); // Limpia la sesión
-            window.location.href = 'loginform.html'; // Redirige al login
+            sessionStorage.removeItem('userEmail'); 
+            window.location.href = 'home.html'; 
         });
     } else {
         const loginForm = document.getElementById('loginForm');
@@ -19,10 +17,10 @@ document.addEventListener('DOMContentLoaded', function() {
             loginForm.addEventListener('submit', function(e) {
                 e.preventDefault();
                 const email = document.getElementById('email').value;
-                // Simple validación de email
+                
                 if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-                    sessionStorage.setItem('userEmail', email); // Guarda el correo en Session Storage
-                    window.location.href = 'home.html'; // Redirige al usuario a la página de inicio
+                    sessionStorage.setItem('userEmail', email);
+                    window.location.href = 'home.html'; 
                 } else {
                     alert('Por favor, introduce un correo electrónico válido.');
                 }
