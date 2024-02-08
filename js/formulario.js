5bb32f5c74ea9f76c13b23354b076894cfacef44
Leslie Velasquez Rosazza <31331188+leslierosazza@users.noreply.github.com>
2024-02-07 23:23:22 -0500

    Add files via upload
    Modificación del formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -5,15 +5,34 @@ if(formContacto){
     formContacto.addEventListener('submit', function(e){
         e.preventDefault();
         const email = document.getElementById('email').value;
+        const nombres = document.getElementById('nombres').value;
+        const apellidos = document.getElementById('apellidos').value;
+        const Textarea1 = document.getElementById('Textarea1').value;
         let text = "Su mensaje ha sido enviado con exito.";
-        let textError = "Por favor, introduce un correo electrónico válido.";
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        let textError1 = "Por favor, introduce un correo electrónico válido.";
+        let textError2 = "Por favor, completa todos los campos.";
+
+        // Validar que los campos no estén vacíos
+        if (nombres === "" || apellidos === "" || email === "" || Textarea1 === "") {
+            document.getElementById("mensajeError").innerHTML = textError2;
+        }
+
+        // Validar el formato del correo electrónico
+        const caracter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (caracter.test(email)) {
             sessionStorage.setItem('userEmail', email);  
             document.getElementById("mensaje").innerHTML = text;
         } else {
-            document.getElementById("mensajeError").innerHTML = textError;
+            document.getElementById("mensajeError").innerHTML = textError1;
         }
     });
+}
+
+function Limpiar(){
+    document.getElementById("nombres").value = "";
+    document.getElementById("apellidos").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("Textarea1").value = "";
 }
 
 function goBack() {
