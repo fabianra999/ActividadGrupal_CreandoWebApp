018894190a99eaac21e74f028c1e32b1255bc979
Leslie Velasquez Rosazza <31331188+leslierosazza@users.noreply.github.com>
2024-02-09 18:58:31 -0500

    Add files via upload
    Actualización del formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -1,41 +1,42 @@
-const formContacto = document.getElementById('formContacto');
-const mensaje = document.getElementById('mensaje');
-const mensajeError = document.getElementById('mensajeError');
-if(formContacto){
-    formContacto.addEventListener('submit', function(e){
-        e.preventDefault();
-        const email = document.getElementById('email').value;
-        const nombres = document.getElementById('nombres').value;
-        const apellidos = document.getElementById('apellidos').value;
-        const Textarea1 = document.getElementById('Textarea1').value;
-        let text = "Su mensaje ha sido enviado con exito.";
-        let textError1 = "Por favor, introduce un correo electrónico válido.";
-        let textError2 = "Por favor, completa todos los campos.";
+document.addEventListener("DOMContentLoaded", () => {
+    const formContacto = document.getElementById('formContacto');
+    
+    if(formContacto){
+        formContacto.addEventListener('submit', function(e){
+            e.preventDefault();
+            const email = document.getElementById('email').value;
+            const nombres = document.getElementById('nombres').value;
+            const apellidos = document.getElementById('apellidos').value;
+            const Textarea1 = document.getElementById('Textarea1').value;
+            const caracter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            // Validar que los campos no estén vacíos
+            if (nombres === "" || apellidos === "" || email === "" || Textarea1 === "") {
+                document.getElementById('mensajeError').setAttribute('class','alert alert-danger');
+                mensajeError.innerText = "Por favor, completa todos los campos.";
+                return false;
+            } else if (caracter.test(email)){       // Validar correo
+                document.getElementById('mensaje').setAttribute('class','alert alert-primary');
+                mensaje.innerText = "Su mensaje ha sido enviado con exito.";
+            } else {
+                document.getElementById('mensajeError').setAttribute('class','alert alert-danger');
+                mensajeError.innerText = "Por favor, introduce un correo electrónico válido.";
+            }
+            formContacto.reset();
+        });
+        // Limpiar los campos
+        /* const callButton = document.querySelector("#btn-limpiar");
+        callButton.addEventListener('click', () => {
+            document.getElementById("nombres").value = "";
+            document.getElementById("apellidos").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("Textarea1").value = "";
+        }); */ 
+    }
 
-        // Validar que los campos no estén vacíos
-        if (nombres === "" || apellidos === "" || email === "" || Textarea1 === "") {
-            document.getElementById("mensajeError").innerHTML = textError2;
-            return false;
-        }
-
-        // Validar el formato del correo electrónico
-        const caracter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if (caracter.test(email)) {
-            sessionStorage.setItem('userEmail', email);  
-            document.getElementById("mensaje").innerHTML = text;
-        } else {
-            document.getElementById("mensajeError").innerHTML = textError1;
-        }
+     // Boton regresar
+    const goBack = document.querySelector("#btn-regresar");
+    goBack.addEventListener('click', () => {
+        window.location.href = 'index.html'; 
     });
-}
+});
 
-function Limpiar(){
-    document.getElementById("nombres").value = "";
-    document.getElementById("apellidos").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("Textarea1").value = "";
-}
-
-function goBack() {
-    window.location.href = 'index.html'; 
-  }
